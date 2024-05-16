d7beaef055239aaa8e130debaa87d2cbd1d88ee8
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-05-16 19:12:20 +0330

    Add files via upload

---
+++ proj1/task.py
@@ -0,0 +1,6 @@
+class Task:
+    def __init__(self, description, due_date):
+        self.description = description
+        self.due_date = due_date
+
+    # Add more task-related methods here
---
+++ proj1/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    # Add more user-related methods here
