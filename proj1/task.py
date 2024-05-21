cdac956fc4d0b17d31f5d205cf4c3ed87529b1d0
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-05-21 13:32:23 +0330

    Delete proj1 directory

--- proj1/task.py
+++
@@ -1,6 +0,0 @@
-class Task:
-    def __init__(self, description, due_date):
-        self.description = description
-        self.due_date = due_date
-
-    # Add more task-related methods here
--- proj1/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    # Add more user-related methods here
